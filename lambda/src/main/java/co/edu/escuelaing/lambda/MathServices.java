c86605b3d0812bdb72ffe15e21b86040980c8e2f
unknown <juan.cancelado-s@labinfo.is.escuelaing.edu.co>
2025-10-15 10:04:15 -0500

    lambda

---
+++ lambda/src/main/java/co/edu/escuelaing/lambda/MathServices.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.escuelaing.lambda;
+
+public class MathServices {
+    
+    public static Integer square(Integer i){
+        return i*i;
+    }
+}
